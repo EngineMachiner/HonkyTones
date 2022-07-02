0db8dc4729c2b4895fc6c3c84663218ac0883aec
EngineMachiner <mfpc98@gmail.com>
2022-07-02 12:59:03 -0500

    Gradle gradle

--- src/main/kotlin/com/enginemachiner/honkytones/Base.kt
+++ src/main/kotlin/com/enginemachiner/honkytones/Base.kt
@@ -96,45 +96,48 @@ private fun addSoundSets() {
 
     // Build a single octave with all notes
     for (note in notes) {
-        if ( note != "C" && note != "F" ) { octave.add(note + "_") }
+        if (note != "C" && note != "F") {
+            octave.add(note + "_")
+        }
         octave.add(note)
     }
 
     // A two octaves list to get relative semitone positions
-    val tempTwo = builder(octave, setOf(1,2))
-    for ( t in tempTwo ) {
-        val s = t.replace("1","").replace("2","")
-        twoOctaves.add( s )
+    val tempTwo = builder(octave, setOf(1, 2))
+    for (t in tempTwo) {
+        val s = t.replace("1", "").replace("2", "")
+        twoOctaves.add(s)
     }
 
     // And a complete whole note set for midi messages references
     val wholeRange = mutableSetOf<Int>()
-    for ( i in -1..8 ) wholeRange.add(i)
+    for (i in -1..8) wholeRange.add(i)
     wholeNoteSet = builder(octave, wholeRange)
 
     // Start adding the set with the sound structure to the map
 
-    val soundsSet4 = setOf("B2-D3","E3_-G3_","G3-B3_","B3-D4")
+    val soundsSet4 = setOf("B2-D3", "E3_-G3_", "G3-B3_", "B3-D4")
 
     // Drum set setup
     val drumSet = builder(octave, setOf(2)) as MutableSet<String>
-    val tempDrumSet = mutableSetOf("C3","D3_","D3","E3_")
-    for (n in tempDrumSet) { drumSet.add(n) }
+    val tempDrumSet = mutableSetOf("C3", "D3_", "D3", "E3_")
+    for (n in tempDrumSet) {
+        drumSet.add(n)
+    }
     soundsMap["drumset"] = drumSet
 
-    soundsMap["organ"] = setOf("C3-E3_","E3-G3","A3_-B3","B3")
+    soundsMap["organ"] = setOf("C3-E3_", "E3-G3", "A3_-B3", "B3")
     soundsMap["harp"] = builder(soundsMap["organ"], setOf(4))
 
     // Violin setup
-    val tempViolin = builder( soundsMap["harp"], setOf(3) ) as MutableSet<String>
-    tempViolin.add( "C6" );      soundsMap["violin"] = tempViolin
+    val tempViolin = builder(soundsMap["harp"], setOf(3)) as MutableSet<String>
+    tempViolin.add("C6"); soundsMap["violin"] = tempViolin
 
-    soundsMap["keyboard"] = builder(octave, setOf(3,4,5,6))
+    soundsMap["keyboard"] = builder(octave, setOf(3, 4, 5, 6))
     soundsMap["electricguitar"] = builder(octave, setOf(4))
-    soundsMap["acousticguitar"] = clone( soundsMap["organ"] )
-    soundsMap["electricguitar-clean"] = clone( soundsMap["organ"] )
-    soundsMap["viola"] = builder(soundsSet4, setOf(3,6))
-    println(soundsMap["viola"])
+    soundsMap["acousticguitar"] = clone(soundsMap["organ"])
+    soundsMap["electricguitar-clean"] = clone(soundsMap["organ"])
+    soundsMap["viola"] = builder(soundsSet4, setOf(3, 6))
     soundsMap["flute"] = clone( soundsMap["harp"] )
     soundsMap["oboe"] = clone( soundsMap["harp"] )
     soundsMap["trombone"] = clone( soundsMap["harp"] )
