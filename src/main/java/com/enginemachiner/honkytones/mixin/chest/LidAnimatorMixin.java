d8044ea37ea84db3d132abc3fd0e22b908228075
EngineMachiner <mfpc98@gmail.com>
2023-11-15 14:14:42 -0500

    bugfixes
    1.19.2 from d51c79c to 9f38735

---
+++ src/main/java/com/enginemachiner/honkytones/mixin/chest/LidAnimatorMixin.java
@@ -0,0 +1,30 @@
+package com.enginemachiner.honkytones.mixin.chest;
+
+import com.enginemachiner.honkytones.LidAnimatorBehaviour;
+import net.minecraft.block.entity.ChestLidAnimator;
+import org.spongepowered.asm.mixin.Mixin;
+
+import static com.enginemachiner.honkytones.UtilityKt.client;
+
+@Mixin(ChestLidAnimator.class)
+public class LidAnimatorMixin implements LidAnimatorBehaviour {
+
+    @Override
+    public void renderStep() {
+
+        LidAnimatorAccessor lid = (LidAnimatorAccessor) this;
+
+        float progress = lid.getProgress();     boolean open = lid.getOpen();
+        float rate = client().getLastFrameDuration() * 0.1f;
+
+        lid.setLastProgress(progress);
+
+        if ( !open && progress > 0.0F ) {
+            lid.setProgress( Math.max( progress - rate, 0.0F ) );
+        } else if ( open && progress < 1.0F ) {
+            lid.setProgress( Math.min( progress + rate, 1.0F ) );
+        }
+
+    }
+
+}
