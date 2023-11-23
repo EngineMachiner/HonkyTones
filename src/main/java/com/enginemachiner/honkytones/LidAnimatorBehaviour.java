9bcb708d795ac722063fb2f4f9f8c1483a71e0c6
EngineMachiner <mfpc98@gmail.com>
2023-11-23 17:42:21 -0500

    Update LidAnimatorBehaviour.java
    Because interface injection happens in the server too?

--- src/main/java/com/enginemachiner/honkytones/LidAnimatorBehaviour.java
+++ src/main/java/com/enginemachiner/honkytones/LidAnimatorBehaviour.java
@@ -1,7 +1,3 @@
 package com.enginemachiner.honkytones;
 
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-
-@Environment(EnvType.CLIENT)
 public interface LidAnimatorBehaviour { void renderStep(); }
