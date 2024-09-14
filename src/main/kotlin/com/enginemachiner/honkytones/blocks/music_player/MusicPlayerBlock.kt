af15bff8d6421bda6e372b2b1d3ba03756220ae3
EngineMachiner <mfpc98@gmail.com>
2024-09-14 10:59:00 -0500

    MusicPlayerBlock small tweaks (c06b97a)

--- src/main/kotlin/com/enginemachiner/honkytones/blocks/music_player/MusicPlayerBlock.kt
+++ src/main/kotlin/com/enginemachiner/honkytones/blocks/music_player/MusicPlayerBlock.kt
@@ -220,16 +220,6 @@ class MusicPlayerBlock(settings: Settings) : BlockWithEntity(settings) {
 
             val builder1 = BlockEntityType.Builder.create( ::MusicPlayerBlockEntity, registerBlock ).build()
 
-
-            /*
-             * This might not be the right solution, but I did this
-             * because of the tick in the block entity that checks new position and gets unsupported blocks.
-             */
-
-            val registries = Registries.BLOCK
-
-            // TODO: for ( i in 0 until registries.size() ) builder1.addBlock( registries[i] )
-
             type = Registry.register( Registries.BLOCK_ENTITY_TYPE, id, builder1 )
 
 
