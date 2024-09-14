5416ea0f0c426bf3781526686b759ac553942e6f
EngineMachiner <mfpc98@gmail.com>
2024-09-14 10:40:20 -0500

    MusicPlayerBlock small tweaks (c06b97a)

--- src/client/kotlin/com/enginemachiner/honkytones/client/blocks/music_player/MusicPlayerBlock.kt
+++ src/client/kotlin/com/enginemachiner/honkytones/client/blocks/music_player/MusicPlayerBlock.kt
@@ -481,13 +481,15 @@ class MusicPlayer( val id: Int ) {
     /** Reads requests asynchronously. Download media using yt-dlp + ffmpeg. */
     private fun read() {
 
-        onQuery = true;     isInputStream = false;      modPrint("$this: Reading...")
+        onQuery = true;     isInputStream = false
 
 
         val isValid = isValidUrl(path);       if ( !isValid ) { sendFile(); return }
 
 
         // Application / octet-stream.
+
+        modPrint("$this: Reading...")
 
         val connection = url().openConnection();        val type = connection.contentType ?: return
 
@@ -543,7 +545,7 @@ class MusicPlayer( val id: Int ) {
 
         val canSend = isMidi() || path.endsWith(".mp3") || path.endsWith(".ogg")
 
-        if ( !canSend || isHost() ) return
+        if ( !canSend || !isHost() ) return
 
 
         val file = file();          if ( !file.exists() ) { warnMissingFile(); return }
