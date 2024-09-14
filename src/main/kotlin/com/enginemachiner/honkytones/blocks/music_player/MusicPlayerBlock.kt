83d2fa5f18539c8ce2549a9b7e03177cd1f4c833
EngineMachiner <mfpc98@gmail.com>
2024-09-14 16:26:17 -0500

    MusicPlayerBlock small tweaks (c06b97a)

--- src/main/kotlin/com/enginemachiner/honkytones/blocks/music_player/MusicPlayerBlock.kt
+++ src/main/kotlin/com/enginemachiner/honkytones/blocks/music_player/MusicPlayerBlock.kt
@@ -220,16 +220,6 @@ class MusicPlayerBlock(settings: Settings) : BlockWithEntity(settings) {
 
             val builder1 = FabricBlockEntityTypeBuilder.create( ::MusicPlayerBlockEntity, registerBlock )
 
-
-            /*
-             * This might not be the right solution, but I did this
-             * because of the tick in the block entity that checks new position and gets unsupported blocks.
-             */
-
-            val registry = Registry.BLOCK
-
-            //for ( i in 0 until registry.size() ) builder1.addBlock( registry[i] )
-
             MusicPlayerBlockEntity.type = Registry.register( Registry.BLOCK_ENTITY_TYPE, id, builder1.build() )
 
 
