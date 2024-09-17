5e3a1efd9b9f4f657aa4b9fcab84942d1e5407ab
EngineMachiner <mfpc98@gmail.com>
2024-09-16 23:01:14 -0500

    Update RecordingScreen.kt

--- src/main/kotlin/com/enginemachiner/honkytones/client/items/console/RecordingScreen.kt
+++ src/main/kotlin/com/enginemachiner/honkytones/client/items/console/RecordingScreen.kt
@@ -121,15 +121,19 @@ class RecordingScreen( private val lastScreen: DigitalConsoleScreen ) : Screen( 
 
             init { init(this);      setPos(pathField);      y -= height() * 1.25f + 1 }
 
+            private val former = text
+
             override fun render( matrices: MatrixStack, color: Int ) {
 
                 val name = pathField.text
                 val directory = directories["midis"]!!.path
-                val path = "$directory/$name"
+                var path = "$directory\\$name"
+
+                if ( !path.endsWith(".mid") ) path += ".mid"
 
                 val isFile = ModFile(path).isFile
 
-                if (isFile) text += " (${Translations.overwrite})"
+                if (isFile) text = former + " (${Translations.overwrite})"
 
                 super.render(matrices, color)
 
