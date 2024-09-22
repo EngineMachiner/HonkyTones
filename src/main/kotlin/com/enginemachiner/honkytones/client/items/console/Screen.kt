07a72180faa560ea400db5d6ab1f622034f77e96
EngineMachiner <mfpc98@gmail.com>
2024-09-22 13:19:14 -0500

    Update Screen.kt

--- src/main/kotlin/com/enginemachiner/honkytones/client/items/console/Screen.kt
+++ src/main/kotlin/com/enginemachiner/honkytones/client/items/console/Screen.kt
@@ -322,7 +322,7 @@ class DigitalConsoleScreen(
 
         try {
 
-            MidiSystem.write(sequence, 0, file);    sendMessage(message)
+            MidiSystem.write(sequence, 0, file);    Message(message).send()
 
         } catch (exception: Exception) {
 
