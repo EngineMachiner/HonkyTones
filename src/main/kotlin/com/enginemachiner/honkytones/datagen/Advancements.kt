e55418e42aad0607d5b2d183c8ea3f53347e2bd0
EngineMachiner <mfpc98@gmail.com>
2024-09-29 14:51:53 -0500

    Update Advancements.kt

--- src/main/kotlin/com/enginemachiner/honkytones/datagen/Advancements.kt
+++ src/main/kotlin/com/enginemachiner/honkytones/datagen/Advancements.kt
@@ -30,7 +30,7 @@ class Advancements( output: FabricDataOutput ) : FabricAdvancementProvider(outpu
 
         Root.build( consumer, "any_instrument" )
 
-        Doot.build(consumer);           Keyboard.build(consumer)
+        Keyboard.build(consumer);           Doot.build(consumer)
 
         addInstruments( percussion, consumer )
         addInstruments( strings, consumer )
